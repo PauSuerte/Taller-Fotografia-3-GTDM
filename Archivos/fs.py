204ed14981ae1c6df61bd174ae84fde053ad950d
DiieVil <dvicvil@teleco.upv.es>
2022-04-24 23:57:20 +0200

    FS

---
+++ Archivos/fs.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+import os
+import glob
+
+def load_images(images):
+    ims= []
+    for i in images:
+        image = cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB)
+        ims.append(image)
+
+    return ims
+
+def align(im1, im2):
+    # IMAGENES EN ESCALA DE GRISES
+    im1_gray = cv2.cvtColor(im1,cv2.COLOR_RGB2GRAY)
+    im2_gray = cv2.cvtColor(im2,cv2.COLOR_RGB2GRAY)
+
+    # TAMAÑO DE LA PRIMERA IMAGEN
+    sz = im1.shape
+
+    warp_mode = cv2.MOTION_TRANSLATION
+    # warp_mode = cv2.MOTION_HOMOGRAPHY
+    # warp_mode = cv2.MOTION_AFFINE
+
+    # DEFINE MATRICES 2x3 o 3x3 E INICIALIZA LA MATRIZ IDENTIDAD
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+        warp_matrix = np.eye(3, 3, dtype=np.float32)
+    else :
+        warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+    # NUMERO DE INTERACCIONES
+    interacciones = 5000;
+
+    # ESPECIFICA EL LIMITE DEL INCREMENTO ENTRE DOS INTERACCIONES
+    termination_eps = 1e-10;
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, interacciones,  termination_eps)
+    (cc, warp_matrix) = cv2.findTransformECC (im1_gray, im2_gray, warp_matrix, warp_mode, criteria)
+
+    if warp_mode == cv2.MOTION_HOMOGRAPHY : 
+        im2_aligned = cv2.warpPerspective(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else :
+        im2_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    return im2_aligned
+
+def align_images(images):
+    im0 = images[len(images)//2]
+    aligned_image_list = []
+    cont = 0
+    for im in images:
+        cont += 1
+        print(f'   ALINEANDO imagen {cont}')
+        aligned_image_list.append(align(im0, im))
+    return aligned_image_list
+
+
+def doLap(image):
+
+    # VALORES A RETOCAR (IDEAL: los dos valores similares e impares)
+    kernel_size = 5         # VENTANA LAPLACIANA (PASO ALTO)
+    blur_size   = 5         # BLUR GAUSSIANO (PASO BAJO)
+
+    blurred = cv2.GaussianBlur(image, (blur_size,blur_size), 0)
+    return cv2.Laplacian(blurred, cv2.CV_64F, ksize=kernel_size)
+
+
+def focus_stack(unimages):
+    images = align_images(unimages)
+
+    print('   Done. FOCUS-STACKING')
+    laps = []
+    for i in range(len(images)):
+        laps.append(doLap(cv2.cvtColor(images[i],cv2.COLOR_BGR2GRAY)))
+
+    laps = np.asarray(laps)
+
+    output = np.zeros(shape=images[0].shape, dtype=images[0].dtype)
+
+    for y in range(0,images[0].shape[0]):
+        for x in range(0, images[0].shape[1]):
+            yxlaps = abs(laps[:, y, x])
+            index = (np.where(yxlaps == max(yxlaps)))[0][0]
+            output[y,x] = images[index][y,x]
+    return output
+
+def main(nameSalida,imagenes,dirname):
+    
+    ims = load_images(imagenes)
+
+    im = focus_stack(ims)
+
+    # GUARDAR IMAGEN - [se guarda como png en el directorio especificado]
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(os.path.join(dirname , nameSalida), im)  
+        
+    print('FINALIZADO')
+    
+    return True
--- Archivos/main.py
+++ Archivos/main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-from tkinter import Label, filedialog, Frame, Toplevel, mainloop, Button
+from tkinter import ANCHOR, NW, Label, filedialog, Frame, Toplevel, mainloop, Button
 import panoramic as pm
+import fs as fs
 import cv2
 from PIL import Image, ImageTk
 
 def getDataPan(nameBox,extension,frame): # Funcion para obtener datos y ejecutar la funcion de paneo
+    global pan_window 
     global imRepre # Declaracion global obligatoria de la representacion de la panoramica en ventana
     imagenes = {} # Diccionario que guarda las imagenes seleccionadas por el usuario
     nameSalida = nameBox.get() + extension.get() # Obtener el nombre de la salida seleccionado por el usuario de la caja de input y añadir la extension
@@ -19,6 +21,26 @@ def getDataPan(nameBox,extension,frame): # Funcion para obtener datos y ejecutar
     flag = pm.progPaneo(nameSalida,numFotos,imagenes,dirname) # Ejecutamos la funcion de paneo, junto a los datos necesarios anteriormente obtenidos
     if flag:
         imRepre = ImageTk.PhotoImage(Image.open(dirname+'/'+nameSalida)) # Mostrar imagen en ventana
+        labelIm = Label(frame, image=imRepre) ; labelIm.pack() # Label necesario para que se muestre la imagen 
+
+def getDataFS(nameBox,extension,frame): # Funcion para obtener datos y ejecutar la funcion de paneo
+    global fs_window 
+    global imRepre # Declaracion global obligatoria de la representacion de la imagen final en ventana
+    imagenes = {} # Diccionario que guarda las imagenes seleccionadas por el usuario
+    nameSalida = nameBox.get() + extension.get() # Obtener el nombre de la salida seleccionado por el usuario de la caja de input y añadir la extension
+    dirname = filedialog.askdirectory() #Seleccion de carpeta donde se dejaran los resultados a traves del buscador
+
+    fsFiles = filedialog.askopenfilenames(title = "Elige las fotos a panear", filetypes=(("png files", "*.png"),("jpg files","*.jpg")))
+    numFotos = len(fsFiles) # Obtener el numero de fotos seleccionadas
+
+    for i in range(numFotos): # Bucle for, en rango de 0 al numero de fotos total anteriormente seleccionado. El usuario seleccionara a traves del buscador las imagenes a panear.
+        imagenes[str(i)] = cv2.imread(fsFiles[i]) # Se guarda el path de las imagenes en el diccionario
+    
+    flag = fs.main(nameSalida,imagenes,dirname) # Ejecutamos la funcion de paneo, junto a los datos necesarios anteriormente obtenidos
+    if flag:
+        print('PASO')
+        imRepre = ImageTk.PhotoImage(Image.open(dirname+'/'+nameSalida)) # Mostrar imagen en ventana
+        print(dirname+'/'+nameSalida)
         labelIm = Label(frame, image=imRepre) ; labelIm.pack() # Label necesario para que se muestre la imagen 
 
 
@@ -48,7 +70,7 @@ def openNewWindow(ventana):
 
         pan_window.protocol("WM_DELETE_WINDOW", lambda: close_window('pan')) # Al cerrar la ventana, se recuperara la ventana principal
 
-        title = tk.Label(pan_window, text = "Creacion de panoramicas") ; title.pack() # Creacion de etiqueta ; colocado de etiqueta
+        title = tk.Label(pan_window, text = "Focus Staking") ; title.pack() # Creacion de etiqueta ; colocado de etiqueta
 
         title_nameSalida = tk.Label(pan_window, text = "Introduzca el nombre del resultado (con extension)") ; title_nameSalida.pack() 
 
@@ -62,9 +84,9 @@ def openNewWindow(ventana):
 
         rJpg = tk.Radiobutton(pan_window, text='jpg', variable=extension, value='.jpg') ; rJpg.pack()
 
-        panBtn = tk.Button(pan_window , text = "Realizar panoramica" , command= lambda: getDataPan(nameBox,extension,frame)) ; panBtn.pack() # Realizar panoramica
+        panBtn = tk.Button(pan_window , text = "Realizar focus staking" , command= lambda: getDataPan(nameBox,extension,frame)) ; panBtn.pack() # Realizar panoramica
 
-        frame = Frame(pan_window, width=300, height=200) ; frame.pack()
+        frame = Frame(pan_window, width=200, height=100) ; frame.pack() 
     
     if ventana == 'fs':
         master.withdraw() # Se esconde la ventana principal
@@ -74,6 +96,22 @@ def openNewWindow(ventana):
         fs_window.protocol("WM_DELETE_WINDOW", lambda: close_window('fs')) # Al cerrar la ventana, se recuperara la ventana principal
 
         titleFs = tk.Label(fs_window, text = "Focus Stacking") ; titleFs.pack() # Creacion de etiqueta ; colocado de etiqueta
+
+        title_nameSalida = tk.Label(fs_window, text = "Introduzca el nombre del resultado (con extension)") ; title_nameSalida.pack() 
+
+        nameBox = tk.Entry(fs_window) ; nameBox.pack() # Caja de introduccion de nombre del resultado
+
+        title_extensions = tk.Label(fs_window, text = "Seleccione la extension deseada") ; title_extensions.pack()
+
+        extension = tk.StringVar() # Variable que guardara el resultado de los radio buttons (extensiones)
+
+        rPng = tk.Radiobutton(fs_window, text='.png', variable=extension, value='.png') ; rPng.pack()
+
+        rJpg = tk.Radiobutton(fs_window, text='jpg', variable=extension, value='.jpg') ; rJpg.pack()
+
+        panBtn = tk.Button(fs_window , text = "Realizar panoramica" , command= lambda: getDataFS(nameBox,extension,frame)) ; panBtn.pack() # Realizar panoramica
+
+        frame = Frame(fs_window, width=200, height=100) ; frame.pack() 
 
     if ventana == 'edit':
         master.withdraw() # Se esconde la ventana principal
